2a085073a2e23053447dbff72d3a17789b40b74d
haseebmalik22 <haseebmalik.info@gmail.com>
2022-11-01 04:36:58 -0700

    1st commit

---
+++ app/src/main/java/com/example/firebasepushednotification/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.firebasepushednotification;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edToken;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edToken = findViewById(R.id.edToken);
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            System.out.println("Fetching FCM registration token failed");
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast
+
+                        System.out.println(token);
+                        Toast.makeText(MainActivity.this,"your device registration token is:" + token, Toast.LENGTH_SHORT).show();
+
+                        edToken.setText(token);
+                    }
+                });
+
+    }
+}
